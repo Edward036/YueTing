55df84845205fcfb099765e634b5cf114d4ab00e
谢秋鹏 <xieqiupeng@gmail.com>
2017-11-01 16:23:28 +0800

    update model

--- src/main/java/com/keyi/yueting/domain/YtAdminLog.java
+++ src/main/java/com/keyi/yueting/domain/YtAdminLog.java
@@ -1,49 +1,47 @@
 package com.keyi.yueting.domain;
 
-import org.hibernate.validator.constraints.NotBlank;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
 
 /**
  * Created by 廖师兄
  * 2016-11-03 23:07
  */
 @Entity
-public class YtAdmin {
+public class YtAdminLog {
 
     @Id
     @GeneratedValue
-//    @Min(value = 18, message = "")
-//    @NotNull
-//    @Max()
-//    @Length()
     private Integer id;
 
-    @NotBlank(message = "这个字段必传")
     @Column(length = 5)
-    private Integer role_id;
+    private Integer admin_id;
 
     @Column(length = 32)
-    private String username;
+    private String admin_name;
 
-    @NotNull(message = "")
     @Column(length = 32)
+    private String admin_ip;
+
+    @Column(length = 32)
+    private String add_time;
+
+    @Column(length = 32)
+
     private String password;
 
-    public YtAdmin() {
+    public YtAdminLog() {
     }
 
     @Override
     public String toString() {
         return "Girl{" +
                 "id=" + id +
-                ", role_id='" + role_id + '\'' +
-                ", username=" + username + '\'' +
-                ", password=" + password + '\'' +
+                ", admin_id='" + admin_id + '\'' +
+                ", admin_name=" + admin_name + '\'' +
+                ", admin_ip=" + admin_ip + '\'' +
                 '}';
     }
 }
