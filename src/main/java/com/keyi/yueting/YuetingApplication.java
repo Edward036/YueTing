ef0c8b2c58abf7560388c8afe9d562ea73a8f929
谢秋鹏 <xieqiupeng@gmail.com>
2017-11-01 12:50:35 +0800

    update yueting

---
+++ src/main/java/com/keyi/yueting/YuetingApplication.java
@@ -0,0 +1,12 @@
+package com.keyi.yueting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuetingApplication.class, args);
+	}
+}
