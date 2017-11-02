4d373cb0b37bd95e6ebd9d6def33edbe4e1dce1e
谢秋鹏 <xieqiupeng@gmail.com>
2017-11-02 10:50:45 +0800

    add models

---
+++ src/main/java/com/keyi/yueting/domain/YtRecordLoginMoney.java
@@ -0,0 +1,49 @@
+package com.keyi.yueting.domain;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by 廖师兄
+ * 2016-11-03 23:07
+ */
+@Entity
+public class YtRecordLoginMoney {
+
+    @Id
+    @GeneratedValue
+//    @Min(value = 18, message = "")
+//    @NotNull
+//    @Max()
+//    @Length()
+    private Integer id;
+
+    @NotBlank(message = "这个字段必传")
+    @Column(length = 5)
+    private Integer role_id;
+
+    @Column(length = 32)
+    private String username;
+
+    @NotNull(message = "")
+    @Column(length = 32)
+    private String password;
+
+    public YtRecordLoginMoney() {
+    }
+
+    @Override
+    public String toString() {
+        return "Girl{" +
+                "id=" + id +
+                ", role_id='" + role_id + '\'' +
+                ", username=" + username + '\'' +
+                ", password=" + password + '\'' +
+                '}';
+    }
+}
