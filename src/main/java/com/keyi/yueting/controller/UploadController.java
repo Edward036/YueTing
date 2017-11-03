1468c1021771d991ffc36512af1578421da9c190
谢秋鹏 <xieqiupeng@gmail.com>
2017-11-03 15:27:43 +0800

    add 评论

--- src/main/java/com/keyi/yueting/controller/UploadController.java
+++ src/main/java/com/keyi/yueting/controller/UploadController.java
@@ -1,4 +1,27 @@
 package com.keyi.yueting.controller;
 
 public class UploadController {
+    private int type = 0;
+    private String[] result = new String[]{};
+    private String id = null;
+    private String host = null;
+    private String base64_policy = null;
+    private String signature = null;
+    private String expire = null;
+    private String oss_path = null;
+    private String oss_key = null;
+
+    private void initUpload(int type) {
+
+    }
+
+    public void picture() {
+    }
+
+    public void audio() {
+    }
+
+    public void gmt_iso8601(String time) {
+
+    }
 }
