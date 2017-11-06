3223b780ebdcf333e555710c05bfb6d7455812a9
谢秋鹏 <xieqiupeng@gmail.com>
2017-11-06 11:53:37 +0800

    update user

--- src/main/java/com/keyi/yueting/domain/YtUser.java
+++ src/main/java/com/keyi/yueting/domain/YtUser.java
@@ -24,66 +24,66 @@ public class YtUser {
     private Integer id;
 
     @NotBlank(message = "这个字段必传")
-    @Column(length = 5)
-    private Integer user_type;
+    @Column(columnDefinition = "int(2) COMMENT '用户类型：1公众号用户'")
 
-    @Column(length = 32)
+    private Integer user_type;
+    @NotNull(message = "")
+    @Column(columnDefinition = "varchar(64) COMMENT '微信openid'")
     private String openId;
 
     @NotNull(message = "")
-    @Column(length = 32)
+    @Column(columnDefinition = "varchar(64) COMMENT '微信昵称'")
     private String nickname;
 
-    @Column(length = 5)
+    @Column(columnDefinition = "int(2) COMMENT '性别'")
     private Integer sex;
 
     @NotNull(message = "")
-    @Column(length = 256)
+    @Column(columnDefinition = "varchar(256) COMMENT '头像URL'")
     private String headImgUrl;
 
     @NotNull(message = "")
-    @Column(length = 64)
+    @Column(columnDefinition = "varchar(64) COMMENT 'unionId'")
     private String unionId;
 
     @NotNull(message = "")
-    @Column(length = 32)
+    @Column(columnDefinition = "varchar(32) COMMENT '国家'")
     private String country;
 
     @NotNull(message = "")
-    @Column(length = 32)
+    @Column(columnDefinition = "varchar(32) COMMENT '省'")
     private String province;
 
     @NotNull(message = "")
-    @Column(length = 32)
+    @Column(columnDefinition = "varchar(32) COMMENT '市'")
     private String city;
 
     @NotNull(message = "")
-    @Column(length = 16)
+    @Column(columnDefinition = "int(16) COMMENT '来源渠道（第一次登录绑定）'")
     private Integer fromChannelId;
 
     @NotNull(message = "")
-    @Column(length = 16)
+    @Column(columnDefinition = "int(16) COMMENT '用户绑定渠道ID（第一次充值绑定）'")
     private Integer channelId;
 
     @NotNull(message = "")
-    @Column(length = 16)
+    @Column(columnDefinition = "int(16) COMMENT '总剩余阅听券'")
     private Integer ticket;
 
     @NotNull(message = "")
-    @Column(length = 16)
+    @Column(columnDefinition = "int(16) COMMENT '总剩余充值阅听卷'")
     private Integer ticketRecharge;
 
     @NotNull(message = "")
-    @Column(length = 16)
+    @Column(columnDefinition = "int(16) COMMENT '总剩余充值赠送阅听卷'")
     private Integer ticketGive;
 
-
     @NotNull(message = "")
-    @Column(length = 16)
+    @Column(columnDefinition = "varchar(16) COMMENT '注册IP'")
     private Integer regIp;
 
     @NotNull(message = "")
-    @Column(length = 16)
+    @Column(columnDefinition = "int(64) COMMENT '注册时间'")
     private Integer regTime;
 
     public YtUser() {
