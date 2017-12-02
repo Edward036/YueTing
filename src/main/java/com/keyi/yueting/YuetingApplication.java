9e6b598ef381a3f4c71b0b1aac856b30cfb067b1
xieqi <xieqiupeng@gmail.com>
2017-12-02 19:31:42 +0800

    update

--- src/main/java/com/keyi/yueting/YuetingApplication.java
+++ src/main/java/com/keyi/yueting/YuetingApplication.java
@@ -5,8 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@ComponentScan("com.keyi.yueting")
 public class YuetingApplication {
 
 	public static void main(String[] args) {
