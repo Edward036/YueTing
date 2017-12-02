de3d0e742223fcb27e780124f2f03edc1b565b1f
201711150039-\xieqi <xieqiupeng@gmail.com>
2017-12-02 19:20:08 +0800

    add jetty

--- src/main/java/com/keyi/yueting/YuetingApplication.java
+++ src/main/java/com/keyi/yueting/YuetingApplication.java
@@ -2,11 +2,22 @@ package com.keyi.yueting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class YuetingApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(YuetingApplication.class, args);
+	}
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		JettyEmbeddedServletContainerFactory factory =
+				new JettyEmbeddedServletContainerFactory();
+		return factory;
 	}
 }
