814a2212c433fd11dec136961e27c366419bd347
xieqi <xieqiupeng@gmail.com>
2017-12-02 20:28:49 +0800

    update

--- src/main/java/com/keyi/yueting/YuetingApplication.java
+++ src/main/java/com/keyi/yueting/YuetingApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory
 import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
+//@EnableJpaRepositories("entity-manager-factory-ref='entityManagerFactory'")
+//@EnableScheduling
 @ComponentScan("com.keyi")
 public class YuetingApplication {
 
